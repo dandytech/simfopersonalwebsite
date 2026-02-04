62b55ec2331752ec0523a8ad571d375457e6bd67
amaechinwankwo <damaechi@rocketmail.com>
2026-02-04 21:24:29 +0100

    Added Craps Game Stats Section

--- crapsgames/crapsgame.js
+++ crapsgames/crapsgame.js
@@ -1,10 +1,20 @@
+//Craps Main Data
+let crapsUsername = "";
+
+//Craps Game Settings
+const startingMoney = 1000;
+const startingRounds = 0;
+
 //HTML Element IDs
 const crapsUsernameInput = "craps-username-input";
 const crapsRegistrationPane = "craps-registration-pane";
 const crapsMainSection = "craps-main-section";
+const crapsStatsUsername = "craps-stats-username";
+const crapsStatsMoney = "craps-stats-money";
+const crapsStatsRounds = "craps-stats-rounds";
 
 function registerCrapsPlayer() {
-  let crapsUsername = document.getElementById(crapsUsernameInput).value;
+  crapsUsername = document.getElementById(crapsUsernameInput).value;
 
   //Username validation check
   let firstCharIsDigitRegex = /^[0-9]|[^a-zA-Z0-9_]/g;
@@ -15,6 +25,7 @@ function registerCrapsPlayer() {
   } else {
     removeRegistrationPane();
     showMainGameSection();
+    setUpFirstRound();
   }
 }
 
@@ -24,4 +35,18 @@ function removeRegistrationPane() {
 
 function showMainGameSection() {
   document.getElementById(crapsMainSection).style.display = "block";
+}
+
+function setUpFirstRound() {
+  document.getElementById(crapsStatsUsername).innerHTML = crapsUsername;
+  setMoney(startingMoney);
+  setRound(startingRounds);
+}
+
+function setMoney(money) {
+  document.getElementById(crapsStatsMoney).innerHTML = money;
+}
+
+function setRound(round) {
+  document.getElementById(crapsStatsRounds).innerHTML = round;
 }
