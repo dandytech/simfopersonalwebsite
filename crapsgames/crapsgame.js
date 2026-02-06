f10d321262d38b9815e56b7fe0a536f9fb7ef505
amaechinwankwo <damaechi@rocketmail.com>
2026-02-06 15:59:29 +0100

    Enabled set bet amount

--- crapsgames/crapsgame.js
+++ crapsgames/crapsgame.js
@@ -8,6 +8,7 @@ const bets = {
   even: "EVEN",
   odd: "ODD",
 };
+const minimumBet = 50;
 
 //HTML Element IDs
 const crapsUsernameInput = "craps-username-input";
@@ -16,11 +17,13 @@ const crapsMainSection = "craps-main-section";
 const crapsStatsUsername = "craps-stats-username";
 const crapsStatsMoney = "craps-stats-money";
 const crapsStatsRounds = "craps-stats-rounds";
+const crapsUserBetAmount = "craps-user-bet-amount";
 
 //In-Game varibales
 let currentMoney = startingMoney;
 let currentRounds = startingRounds;
 let currentBet = bets.even;
+let currentBetAmount = minimumBet;
 
 function registerCrapsPlayer() {
   crapsUsername = document.getElementById(crapsUsernameInput).value;
@@ -53,6 +56,7 @@ function setUpFirstRound() {
   setMoney(currentMoney);
   setRound(currentRounds);
   betEven();
+  setBetAmount(minimumBet);
 }
 
 function setMoney(money) {
@@ -74,4 +78,16 @@ function choseBet(bet) {
   document.getElementById(bet).style.backgroundColor = "red";
   const deselectBet = bet == bets.even ? bets.odd : bets.even;
   document.getElementById(deselectBet).style.backgroundColor = "transparent";
+}
+
+function increaseBet() {
+  setBetAmount(Math.min(currentBetAmount + minimumBet, currentMoney));
+}
+function decreaseBet() {
+  setBetAmount(Math.max(currentBetAmount - minimumBet, minimumBet));
+}
+
+function setBetAmount(betAmount) {
+  currentBetAmount = betAmount;
+  document.getElementById(crapsUserBetAmount).innerHTML = "$" + betAmount;
 }
