a5febfea99b35bd2fe1819b2871566e3a3353966
amaechinwankwo <damaechi@rocketmail.com>
2026-02-14 22:37:43 +0100

    Added vite custom steps

--- personal-website/vite.config.ts
+++ personal-website/vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "./src/crapsgames/crapsgame.js", // source file
+          dest: "./src/crapsgames/", // will be placed in dist/crapsgames/
+        },
+        {
+          src: "src/randomQuoteGenerator/randQuoteGenerator.js", // source file
+          dest: "randomQuoteGenerator", // will be placed in dist/randomQuoteGenerator/
+        },
+        {
+          src: "./src/images/cv.pdf", // source file
+          dest: "./src/images/", // will be placed in dist/crapsgames/
+        },
+      ],
+    }),
+  ],
+});
