162bccea60b943ccae461bb02c538dbb4222d08b
amaechinwankwo <damaechi@rocketmail.com>
2026-02-14 12:46:36 +0100

    Added reusable numberStat component

--- personal-website/src/stockAnalysisDashboard/dashboardGrid.tsx
+++ personal-website/src/stockAnalysisDashboard/dashboardGrid.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import "gridstack/dist/gridstack.min.css";
 import { GridStack } from "gridstack";
 import { DashboardGridContent } from "./stockAnalysisDashboard";
+import NumberStat from "./numberStat";
 
 function DashboardGrid({ stockData }: { stockData: any }) {
   useEffect(() => {
@@ -15,26 +16,37 @@ function DashboardGrid({ stockData }: { stockData: any }) {
         {/* First row */}
         <div className="grid-stack-item" gs-w="3">
           <DashboardGridContent className="grid-stack-item-content">
-            <div> {stockData.basicInfo.marketCap}</div>
-            <div>Market Cap</div>
+            <NumberStat
+              value={stockData.basicInfo.marketCap}
+              label="Market Cap"
+              center={true}
+            ></NumberStat>
           </DashboardGridContent>
         </div>
         <div className="grid-stack-item" gs-w="3">
           <DashboardGridContent className="grid-stack-item-content">
-            <div> {stockData.basicInfo.fullTimeEmployees}</div>
-            <div>Employees</div>
+            <NumberStat
+              value={stockData.basicInfo.fullTimeEmployees}
+              label="Employees"
+              center={true}
+            ></NumberStat>
           </DashboardGridContent>
         </div>
         <div className="grid-stack-item" gs-w="3">
           <DashboardGridContent className="grid-stack-item-content">
-            <div> {stockData.basicInfo.totalRevenue}</div>
-            <div>Total Revenue</div>
+            <NumberStat
+              value={stockData.basicInfo.totalRevenue}
+              label="Total Revenue"
+              center={true}
+            ></NumberStat>
           </DashboardGridContent>
         </div>
         <div className="grid-stack-item" gs-w="3">
           <DashboardGridContent className="grid-stack-item-content">
-            <div> {stockData.basicInfo.trailingPE}</div>
-            <div>Earnings Per Share</div>
+            <NumberStat
+              value={stockData.basicInfo.trailingPE}
+              label="Earnings Per Share"
+            ></NumberStat>
           </DashboardGridContent>
         </div>
       </div>
