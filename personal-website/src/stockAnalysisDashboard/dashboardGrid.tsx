dce273c848cf24920c25c6fc0fee0243c430d553
amaechinwankwo <damaechi@rocketmail.com>
2026-02-14 14:10:51 +0100

    Added future earnings date as list rendering

--- personal-website/src/stockAnalysisDashboard/dashboardGrid.tsx
+++ personal-website/src/stockAnalysisDashboard/dashboardGrid.tsx
@@ -60,6 +60,14 @@ function DashboardGrid({ stockData }: { stockData: any }) {
             ></LineChartContent>
           </DashboardGridContent>
         </div>
+        <div className="grid-stack-item" gs-w="2" gs-h="2">
+          <DashboardGridContent className="grid-stack-item-content">
+            <div style={{ marginBottom: "10px" }}>Feature Earnings</div>
+            {stockData.futureEarningDates.map((nextDate: string) => (
+              <div>{nextDate}</div>
+            ))}
+          </DashboardGridContent>
+        </div>
       </div>
     </div>
   );
