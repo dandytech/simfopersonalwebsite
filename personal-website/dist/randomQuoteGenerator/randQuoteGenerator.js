a50c742e556a89f96416f85986c920c5099e3cb8
amaechinwankwo <damaechi@rocketmail.com>
2026-02-15 16:27:56 +0100

    Completed Stock Analysis Dashboard

---
+++ personal-website/dist/randomQuoteGenerator/randQuoteGenerator.js
@@ -0,0 +1,48 @@
+// randQuoteGenerator.js
+const quoteTextElement = "random-quote-text";
+const quoteAuthorElement = "random-quote-author";
+const randomQuoteGeneratorElement = "random-quote-generator";
+
+const colors = [
+  ["#0c7dee", "#db9d9d"],
+  ["#e6e677", "#773a51"],
+  ["#3e1010", "#3c08fa"],
+  ["#bf54bd", "#12a8d1"],
+  ["#0f5003", "#f8d800"],
+  ["#5a0972", "#1752aa"],
+  ["#3d0303", "#08d0c3"],
+  ["#31c93e", "#abd124"],
+  ["#111921", "#193426"],
+  ["#d08011", "#bbbb18"],
+];
+
+function getRandomColorCombo() {
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  return colors[randomIndex];
+}
+
+export async function getNewRandomQuote() {
+  try {
+    const response = await fetch("https://dummyjson.com/quotes/random");
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch quote");
+    }
+
+    const data = await response.json();
+    const quoteAuthor = data.author;
+    const quoteText = data.quote;
+
+    document.getElementById(quoteTextElement).innerHTML = quoteText;
+    document.getElementById(quoteAuthorElement).innerHTML = quoteAuthor;
+
+    const colorCombo = getRandomColorCombo();
+    document.getElementById(randomQuoteGeneratorElement).style.background =
+      `linear-gradient(45deg, ${colorCombo[0]}, ${colorCombo[1]})`;
+  } catch (error) {
+    console.error(error);
+    document.getElementById(quoteTextElement).innerHTML =
+      "Quotes unavailable 😔";
+    document.getElementById(quoteAuthorElement).innerHTML = "";
+  }
+}
